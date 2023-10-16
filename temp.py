26681bf0327a683114c4acc656967470292041fc
Bishwas Shrestha <bishwas.shrestha@gmail.com>
2023-10-16 15:28:30 +0300

    temp.py tidy up

--- temp.py
+++ temp.py
@@ -2,7 +2,7 @@ import machine
 import time
 from oled import OLED
 
-oled = OLED()
+# oled = OLED()
 def get_temp():
     #reading temperature from a sensor
     sensor_temp = machine.ADC(4)
@@ -12,11 +12,11 @@ def get_temp():
     while True:
         reading = sensor_temp.read_u16() * conversion_factor
         temperature = 27-(reading -0.706)/0.001721
- 
-        oled.clear()
-        oled.write('Current Temp', 1,0)
-        oled.write(str(round(temperature,2))+'C',1,20)
-        oled.show()
+#  
+#         oled.clear()
+#         oled.write('Current Temp', 1,0)
+#         oled.write(str(round(temperature,2))+'C',1,20)
+#         oled.show()
        
         print(index,' raw data reading :', reading, ' temperature:', temperature)
         index+=1        
