80db333816b8190456d846ed4e6d2ba6a832a7bf
Bishwas Shrestha <bishwas.shrestha@gmail.com>
2023-10-16 16:07:27 +0300

    publish via http

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ topic_pub = b'temperature'
 topic_msg = b'temp: %s C'%sensor
 
 init_wifi()
-# serve_webpage()
-
-try:
-    client = mqtt_connect()
-except OSError as e:
-    reconnect()
-    
-while True:
-    if sensor != '':
-        LED.on()
-        print('data publishing %s'%topic_msg)        
-        client.publish(topic_pub, topic_msg)        
-        time.sleep(5)       
-       
-    else:
-        pass
-    
+serve_webpage()
+# 
+# try:
+#     client = mqtt_connect()
+# except OSError as e:
+#     reconnect()
+#     
+# while True:
+#     if sensor != '':
+#         LED.on()
+#         print('data publishing %s'%topic_msg)        
+#         client.publish(topic_pub, topic_msg)        
+#         time.sleep(5)       
+#        
+#     else:
+#         pass
+#     
